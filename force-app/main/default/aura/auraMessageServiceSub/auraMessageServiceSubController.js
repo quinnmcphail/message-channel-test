f8025da5dbad94ca8fe8464f865efa6eda5336ce
Quinn McPhail <quinnmcphail@me.com>
2020-03-13 15:00:12 -0400

    Added Home Page

--- force-app/main/default/aura/auraMessageServiceSub/auraMessageServiceSubController.js
+++ force-app/main/default/aura/auraMessageServiceSub/auraMessageServiceSubController.js
@@ -1,5 +1,7 @@
 ({
-    myAction : function(component, event, helper) {
-
+  onMessage: function(cmp, message, helper) {
+    if (message != null) {
+      cmp.set("v.message", JSON.stringify(message, null, "\t"));
     }
-})
+  }
+});
